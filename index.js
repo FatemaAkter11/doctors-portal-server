ab305e708472067492634c252bdf6c765c0212cf
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-03 19:03:59 +0600

    doctores portal mongodb uri added

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pjjkl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctors Portal Server')
+})
+
+app.listen(port, () => {
+    console.log('Listening at doctors portal', port);
+})
