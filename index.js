a901fde3344482f2ae1832f8fef9ea0055a3029a
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-03 19:31:52 +0600

    database connected successfully

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+async function run() {
+    try {
+        await client.connect();
+        console.log('database connected successfully');
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
     res.send('Hello Doctors Portal Server')
 })
