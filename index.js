fc47db5f6fb690cb39a22c16b733609a7734d054
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-11-07 23:04:00 +0600

    Upsert users data to insert if not exists to the database

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ async function run() {
             res.json(result);
         });
 
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
     }
 
     finally {
